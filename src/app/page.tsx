5ebb1c5b67b881c08407ca7dd3b6ff0f24b5d66b
IvanovJenyIT <ivanovjenyit@gmail.com>
2024-02-02 21:57:38 +0200

    first commite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Click me</Button>
+      <Button>Click me1</Button>
     </main>
   );
 }
