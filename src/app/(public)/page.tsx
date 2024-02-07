eae536039254901fd5ab33a9ae47894c98cb0e20
IvanovJenyIT <ivanovjenyit@gmail.com>
2024-02-07 22:45:22 +0200

    Added Header

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,11 +1,14 @@
-import { Button } from "@/components/ui/button";
 import { CoursesList } from "@/features/courses-list/pub/courses-list";
 import { CreateCourseForm } from "@/features/courses-list/pub/create-courses-form";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-8">
-      <CreateCourseForm revalidatePagePath="/" className="w-[300px] mb-5" />
+    <main className="flex min-h-screen flex-col  p-8">
+      <h1 className="text-3xl mb-2">Courses</h1>
+      <CreateCourseForm
+        revalidatePagePath="/"
+        className="max-w-[300px] mb-10"
+      />
       <CoursesList revalidatePagePath="/" />
     </main>
   );
