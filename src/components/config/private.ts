d18e782b9c77a1e4981d002b6a4a370d0464126e
IvanovJenyIT <ivanovjenyit@gmail.com>
2024-02-08 20:16:07 +0200

    Added emal

--- src/components/config/private.ts
+++ src/components/config/private.ts
@@ -7,11 +7,11 @@ const privateConfigSchema = z.object({
   GITHUB_ID: z.string().optional(),
   GITHUB_SECRET: z.string().optional(),
 
-  // EMAIL_SERVER_USER: z.string(),
-  // EMAIL_SERVER_PASSWORD: z.string(),
-  // EMAIL_SERVER_HOST: z.string(),
-  // EMAIL_SERVER_PORT: z.string(),
-  // EMAIL_FROM: z.string(),
+  EMAIL_SERVER_USER: z.string(),
+  EMAIL_SERVER_PASSWORD: z.string(),
+  EMAIL_SERVER_HOST: z.string(),
+  EMAIL_SERVER_PORT: z.string(),
+  EMAIL_FROM: z.string(),
 
   // S3_ACCESS_KEY_ID: z.string(),
   // S3_SECRET_ACCESS_KEY: z.string(),
